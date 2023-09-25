7e55115ecdce10e033845b2eee878e8dae146436
zfdx <2915441170@qq.com>
2023-09-25 21:43:36 +0800

    fix: golang version

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,3 +2,8 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+# golang version to openwrt packages
+pushd feeds/packages/lang
+rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-23.05/lang/golang
+popd
