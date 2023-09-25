6e2102fa1ae4ec09bac7aed0306ab68f0651e43d
zfdx <2915441170@qq.com>
2023-09-25 21:23:03 +0800

    add xradio xr819

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+mkdir package/myapp
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+cp -rf $GITHUB_WORKSPACE/package/xradio package/myapp/xradio
+
+git clone --depth=1 https://github.com/kenzok78/luci-app-adguardhome package/myapp/luci-app-adguardhome
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/myapp/luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-app-argon-config package/myapp/luci-app-argon-config
+
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
