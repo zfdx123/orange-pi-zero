6fd3b724b1f4b2feab63724d9ebed234e54a8995
zfdx <2915441170@qq.com>
2023-10-03 09:01:57 +0800

    fix xr819

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,6 +4,8 @@ mkdir package/myapp
 
 cp -rf $GITHUB_WORKSPACE/package/xradio package/myapp/xradio
 
+cp -rf $GITHUB_WORKSPACE/kernelPatch/001-fix-wifi-xr819.patch target/linux/sunxi/patches-5.10/
+
 git clone --depth=1 https://github.com/kenzok78/luci-app-adguardhome package/myapp/luci-app-adguardhome
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/myapp/luci-theme-argon
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-app-argon-config package/myapp/luci-app-argon-config
